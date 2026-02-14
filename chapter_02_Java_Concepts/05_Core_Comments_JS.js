6d5feac49cd7fcf8b8b3cb13ba62cf97d05e4b6a
meetijha <meetijha@gmail.com>
2026-02-14 12:34:45 +0530

    LearningPlaywright-14th Feb

---
+++ chapter_02_Java_Concepts/05_Core_Comments_JS.js
@@ -0,0 +1,22 @@
+// This is comment. -This code will not be execute
+console.log("Hello");
+/** 
+ *  This is multi line
+ *  Author : Meeti Jha
+ *  Date : 14-Feb-2026
+ **/
+
+/* 
+ *  This is multi line
+ *  Author : Meeti Jha
+ *  Date : 14-Feb-2026
+ */
+
+//Short cut MAC- command + /
+//Short cut Windows- Ctrl + /
+
+//Multiline comment shortcut
+/* 
+* MAC- Shift + Option + A
+* Windows- Shift + Alt + A 
+*/
