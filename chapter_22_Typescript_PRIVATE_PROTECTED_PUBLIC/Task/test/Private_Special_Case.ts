198debcc98a034a3ec9241abb2d81e0d2e4ba72a
meetijha <meetijha@gmail.com>
2026-04-17 15:48:18 +0530

    private twist

--- chapter_22_Typescript_PRIVATE_PROTECTED_PUBLIC/Task/test/Private_Special_Case.ts
+++ chapter_22_Typescript_PRIVATE_PROTECTED_PUBLIC/Task/test/Private_Special_Case.ts
@@ -1,9 +1,13 @@
 class A {
   private secret = "real private";
+  //To behave like Java, you must use # so that special case doesnt work
+  #secret2= "truly private";
 }
 
 const a= new A();
 //console.log(a.secret); // ❌ Error: Property 'secret' is private and only accessible within class 'A'.
+//console.log(a.#secret2); // ❌ Error: Property '#secret2' is private and only accessible within class 'A'.
 
 const specialA: any = new A();
 console.log(specialA.secret); // works, but this is a special case where we are using 'any' type to bypass TypeScript's access control.
+console.log(specialA.secret2); // undefined, because #secret2 is truly private and cannot be accessed even with 'any' type.
